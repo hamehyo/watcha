42f9ccfd90efa6d9f1330b9a461c31336865d28b
hamehyo <33iamhhs33@gmail.com>
2022-01-20 18:43:30 +0900

    re

--- src/components/PosterboxInner.js
+++ src/components/PosterboxInner.js
@@ -7,7 +7,6 @@ import styles from '../containers/Main.module.scss';
 const cx = className.bind(styles);
 
 function audienceNum(audience){
-    
     if( parseInt(audience) < 10000){
         if( audience.length <= 3){
             return `${audience}명`;
@@ -17,16 +16,10 @@ function audienceNum(audience){
             return `${result},${remainder}명`;
         }
     }else if(parseInt(audience) >= 10000){
-        if(audience === 10000 ){
-            return `1만명`
-        }else{
-            const result1=  parseInt(audience/ 10000);
-            return `${result1}만명`;
-        }
-
+        const result1=  parseInt(audience/ 10000);
+        return `${result1}만명`;
     }
     return audience;
-    
 }
 
 const PosterboxInner = ({idx, img, main_title, sub_title, year, country, reserve, rate, audience}) => {
@@ -41,9 +34,12 @@ const PosterboxInner = ({idx, img, main_title, sub_title, year, country, reserve
                 <img src={`${process.env.PUBLIC_URL}/img/${img}`} alt={main_title} />
             </div>
             <div className={cx('txt_box')}>
-                <p className={cx('tit')}>{ main_title }{sub_title ? ` : ${sub_title}` : ''}</p>
+                <p className={cx('tit')}>{ main_title }{sub_title && ` : ${sub_title}`}</p>
                 <p className={cx('info')}>{ year }・{ country }</p>
-                <p className={cx('rate')}>평균<em>★</em>{ rate }</p>
+                {/* {
+                    rate ? <p className={cx('rate')}>평균<em>★</em>{ rate }</p> : ''
+                } */}
+                {rate && <p className={cx('rate')}>평균<em>★</em>{ rate }</p>}
                 <p className={cx('num')}>예매율 { reserve }%・ 누적 관객 { audienceNum(audience) }</p>
             </div>
         </>
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -9,10 +9,10 @@ import { Paths } from '../paths';
 const MoviePage = () => {
     return(
         <>
-            <ul>
+            {/* <ul>
                 <li><Link to={Paths.main}>메인</Link></li>
                 <li><Link to={Paths.detail}>상세페이지</Link></li>
-            </ul>
+            </ul> */}
             <Switch>
                 <Route path={Paths.main} component={Main} exact />
                 <Route path={Paths.detail} component={Detail}/>
