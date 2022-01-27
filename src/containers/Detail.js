02f5ab262be40b85f8c8d83594a7f8d3792a19fc
hamehyo <33iamhhs33@gmail.com>
2022-01-27 18:37:32 +0900

    error

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -28,35 +28,36 @@ function audienceNum(audience){
 const Detail = ({match}) => {
     const {idx} = match.params;
     //console.log(idx);
-    const movieList = datas.filter((data, index) => (index+1) === Number(idx));
+    const { img, main_title, sub_title, year, country, reserve, rate, audience } = datas.filter((data, index) => (index+1) === Number(idx))[0];
+    console.log(img, main_title, sub_title, year, country, reserve, rate, audience)
     //console.log(movieList)
     return(
         <>
             <section className={cx('detail_container')}>
                 {
-                    movieList.map( (movie) => {
-                        const { img, main_title, sub_title, year, country, reserve, rate, audience } = movie;
-                        return(
-                            <>
-                                <div className={cx('top_main_wrap')} style={{'backgroundImage': `${process.env.PUBLIC_URL}/img/${img}`}}>Detail {idx}</div>
-                                <div className={cx('btm_info_wrap')}>
-                                    <div className={cx('poster_img_box')}>
-                                        <img src={`${process.env.PUBLIC_URL}/img/${img}`} alt={main_title} />
-                                    </div>
-                                    <div className={cx('poster_info_box')}>
-                                        <div className={cx('num')}>
-                                            <span>예매율 <strong>{ reserve }%</strong></span>
-                                            <span>개봉</span> 
-                                            <span>누적관객 <strong>{ audienceNum(audience) }</strong></span>
-                                        </div>
-                                        <div className={cx('tit')}>{main_title}{sub_title && ` : ${sub_title}`}</div>
-                                        <div className={cx('info')}>{ year }・{ country }</div>
-                                        <div className={cx('user_area')}></div>
-                                    </div>
-                                </div>
-                            </>
-                        );
-                    })
+                    // movieList.map( (movie) => {
+                    //     const { img, main_title, sub_title, year, country, reserve, rate, audience } = movie;
+                    //     return(
+                    //         <>
+                    //             <div className={cx('top_main_wrap')} style={{'backgroundImage': `${process.env.PUBLIC_URL}/img/${img}`}}>Detail {idx}</div>
+                    //             <div className={cx('btm_info_wrap')}>
+                    //                 <div className={cx('poster_img_box')}>
+                    //                     <img src={`${process.env.PUBLIC_URL}/img/${img}`} alt={main_title} />
+                    //                 </div>
+                    //                 <div className={cx('poster_info_box')}>
+                    //                     <div className={cx('num')}>
+                    //                         <span>예매율 <strong>{ reserve }%</strong></span>
+                    //                         <span>개봉</span> 
+                    //                         <span>누적관객 <strong>{ audienceNum(audience) }</strong></span>
+                    //                     </div>
+                    //                     <div className={cx('tit')}>{main_title}{sub_title && ` : ${sub_title}`}</div>
+                    //                     <div className={cx('info')}>{ year }・{ country }</div>
+                    //                     <div className={cx('user_area')}></div>
+                    //                 </div>
+                    //             </div>
+                    //         </>
+                    //     );
+                    // })
                 }   
             </section>
             
